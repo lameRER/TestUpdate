f4bd37c69084847f40707fbf5e72b5ca73fe07d3
lameR <erpresents@gmail.com>
2023-02-06 23:38:54 +0300

    Test9

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import os
 
 # os.system('git pull')
 # os.popen('git fetch')
-if 'origin/master' in os.popen('git fetch').read():
+if 'master' in os.popen('git fetch').read():
     os.popen('git pull')
 
 os.system('py main.py')
