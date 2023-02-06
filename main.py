7f20e76b71d655a02971e8217e386ac41dfa232c
lameR <erpresents@gmail.com>
2023-02-07 00:03:52 +0300

    Test10

--- main.py
+++ main.py
@@ -9,4 +9,5 @@ for i in tqdm(range(1000), desc='STEP 1'):
         for j in tqdm(range(1000), leave=False, desc='STEP 3'):
             for j in tqdm(range(1000), leave=False, desc='STEP 4'):
                 for j in tqdm(range(1001), leave=False, desc='STEP 5'):
-                    time.sleep(0.00001)
+                    for j in tqdm(range(1001), leave=False, desc='STEP 6'):
+                        time.sleep(0.00001)
--- update.py
+++ update.py
@@ -7,7 +7,7 @@ os.system('git fetch')
 status = os.popen('git status').read()
 if 'git pull' in status:
     os.popen('git pull')
+    time.sleep(1)
     # print("yes")
 
-time.sleep(1)
 os.system('py main.py')
