b3e47dec1f486920f258018764f1953e0416ddd9
lameR <erpresents@gmail.com>
2023-02-06 23:58:29 +0300

    Test10

--- update.py
+++ update.py
@@ -2,10 +2,9 @@ import os
 
 # os.system('git pull')
 # os.popen('git fetch')
-fetch = os.popen('git fetch').read()
+os.system('git fetch')
 status = os.popen('git status').read()
-print(fetch)
-if 'master' in fetch:
+if 'git pull' in status:
     os.popen('git pull')
     print("yes")
 
