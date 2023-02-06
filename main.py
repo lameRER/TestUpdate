9a52fbc2808a781ec3dbc803831c17093cdd7c53
lameR <erpresents@gmail.com>
2023-02-06 23:18:23 +0300

    Test3

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import os
 import time
 
 from tqdm import tqdm
 
-os.system('git pull')
 
 for i in tqdm(range(1000)):
     for j in tqdm(range(1000)):
---
+++ update.py
@@ -0,0 +1,5 @@
+import os
+
+os.system('git pull')
+
+os.system('py main.py')
