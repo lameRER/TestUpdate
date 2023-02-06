b549bdc8caabec4261cbd990dc99cc6de1d24447
lameR <erpresents@gmail.com>
2023-02-06 23:35:36 +0300

    Test8

--- update.py
+++ update.py
@@ -2,7 +2,7 @@ import os
 
 # os.system('git pull')
 # os.popen('git fetch')
-if 'forced updat' in os.popen('git fetch').read():
+if 'origin/master' in os.popen('git fetch').read():
     os.popen('git pull')
 
 os.system('py main.py')
