a3858a4e4e5ced608c664ae293619fe917cdcbae
lameR <erpresents@gmail.com>
2023-02-06 23:31:18 +0300

    Test6

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from tqdm import tqdm
 
 for i in tqdm(range(1000)):
     for j in tqdm(range(1000), leave=False):
-        time.sleep(0.001)
+        time.sleep(0.1)
--- update.py
+++ update.py
@@ -3,6 +3,6 @@ import os
 # os.system('git pull')
 # os.popen('git fetch')
 if 'forced update' in os.popen('git fetch').read():
-    os.system('git pull')
+    os.popen('git pull')
 
 os.system('py main.py')
