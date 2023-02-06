25d9756a73f5fb23251ea5886726bd23f8ade7ca
lameR <erpresents@gmail.com>
2023-02-07 00:11:34 +0300

    Test10

--- update.py
+++ update.py
@@ -1,13 +1,16 @@
 import os
 import time
+from subprocess import run, STDOUT, PIPE
 
 # os.system('git pull')
 # os.popen('git fetch')
-os.system('git fetch')
-status = os.popen('git status').read()
-if 'git pull' in status:
-    os.popen('git pull')
-    time.sleep(1)
+# os.system('git fetch')
+# status = os.popen('git status').read()
+output = run('git fetch', stderr=STDOUT, stdout=PIPE, text=True)
+if 'master' in output:
+    out = run('git pull', stderr=STDOUT, stdout=PIPE, text=True)
+    print(out)
+    # time.sleep(1)
     # print("yes")
 
-os.system('py main.py')
+# os.system('py main.py')
