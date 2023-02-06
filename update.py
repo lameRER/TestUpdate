12b7e22ac76856f37bb9d8191e104bc8d3b1be78
lameR <erpresents@gmail.com>
2023-02-07 00:16:13 +0300

    Test10

--- update.py
+++ update.py
@@ -6,7 +6,7 @@ from subprocess import run, STDOUT, PIPE
 # os.system('git fetch')
 # status = os.popen('git status').read()
 output = run('git fetch', stderr=STDOUT, stdout=PIPE, text=True)
-if 'origin\master' in output.stdout:
+if 'origin/master' in output.stdout:
     out = run('git pull', stderr=STDOUT, stdout=PIPE, text=True)
     print(out.stdout)
     # time.sleep(1)
