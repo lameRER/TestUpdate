5f276dc4c80bc652dde769b6ddec47e78ffafc3d
lameR <erpresents@gmail.com>
2023-02-06 23:32:34 +0300

    Test7

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import time
+# from colorama import Fore
 
 from tqdm import tqdm
 
 
-for i in tqdm(range(1000)):
-    for j in tqdm(range(1000), leave=False):
+for i in tqdm(range(1000), desc='STEP 1'):
+    for j in tqdm(range(1000), leave=False, desc='STEP 2'):
         time.sleep(0.1)
